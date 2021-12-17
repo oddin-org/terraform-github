db28388f66478af1aaeb2a64a44d04ff1e502101
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2021-12-17 00:11:16 -0300

    Use for_each to create similar resources

---
+++ modules/repository/output.tf
@@ -0,0 +1,9 @@
+output "repo" {
+	description = "Repository that was created"
+	value = github_repository.repo
+}
+
+output "branch" {
+	description = "Default branch for your repo"
+	value = github_branch.branch
+}
